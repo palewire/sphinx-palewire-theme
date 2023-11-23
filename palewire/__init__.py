4a5f1b5f38fc3d12a2c2b11086491ffe6f3e65aa
palewire <b@palewi.re>
2023-11-23 07:09:43 -0500

    Cut old stuff

--- palewire/__init__.py
+++ palewire/__init__.py
@@ -1,8 +1,5 @@
 import os
 
-__version_info__ = (0, 7, 13)
-__version__ = "0.7.13"
-
 
 def get_path():
     """
@@ -12,13 +9,9 @@ def get_path():
     return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
-def update_context(app, pagename, templatename, context, doctree):
-    context["alabaster_version"] = __version__
-
-
 def setup(app):
     app.require_sphinx("1.6")
     theme_path = os.path.abspath(os.path.dirname(__file__))
-    app.add_html_theme("alabaster", theme_path)
+    app.add_html_theme("palewire", theme_path)
     app.connect("html-page-context", update_context)
-    return {"version": __version__, "parallel_read_safe": True}
+    return {"parallel_read_safe": True}
